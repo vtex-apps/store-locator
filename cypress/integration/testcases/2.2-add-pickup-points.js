0c6b5eb18d607e902b1dcda8d4e2ed3d900a0c39
saravanan <saravananvenkatesan@bitcot.com>
2022-05-24 13:45:32 +0530

    added pickupoint & verified

---
+++ cypress/integration/testcases/2.2-add-pickup-points.js
@@ -0,0 +1,25 @@
+import { testSetup, updateRetry, preserveCookie } from '../../support/common/support'
+import {
+  addPickUpPoint,
+  verifyPickUpPoint,
+  verifyAllPickUpPoint
+} from '../../support/storelocator.common'
+
+describe('Testing PickUpPoint in storeLocator', () => {
+  // Load test setup
+  testSetup(true)
+
+  it('Adding PickUp Point in Store-Locator', updateRetry(3), () => {
+    addPickUpPoint()
+  })
+
+  it('Verifying pickup point in store-locator', updateRetry(3), () => {
+    verifyPickUpPoint()
+  })
+
+  it('Verifying all stores by clicking more items', updateRetry(3), () => {
+    verifyAllPickUpPoint()
+  })
+
+  preserveCookie()
+})
---
+++ cypress/support/storelocator.selectors.js
@@ -0,0 +1,16 @@
+export default {
+    AddPickUpButton: '.w-70-ns > :nth-child(2) > .vtex-button__label',
+    PickUpPointName: '.w-60-ns > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input',
+    PickUpPointId: '.pl5-ns > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input',
+    PickUpAddress: '.w-70-m > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input',
+    CheckBox: ':nth-child(3) > .w-40-m > .vtex-checkbox__line-container > .vtex-checkbox__container > .vtex-checkbox__input',
+    WorkStartTime: ':nth-child(3) > .flex-ns > :nth-child(1) > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input',
+    WorkEndTime: ':nth-child(3) > .flex-ns > :nth-child(3) > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input',
+    SaveChanges: '.flex.justify-end > .pb7 > .vtex-button > .vtex-button__label',
+    ChangesSaved: 'div[class*=vtex-toast] .pr5',
+    ListOfStores: '.overflow-auto > .list',
+    LoadStores: '.vtex-store-locator-0-x-loadAll',
+    MoreItems: '.vtex-store-locator-0-x-addressListLink',
+    Hours: '.vtex-store-locator-0-x-hoursContainer',
+    BackToPickUpPoint: '.vtex-store-locator-0-x-backlink'
+}
