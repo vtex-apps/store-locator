9b75dff97697ffe2a6e4725bbe92c23c27ac889c
Shashidhar Reddy <shashidhargujjula@gmail.com>
2022-11-30 13:10:01 +0530

    Increased hard retries to verify pickup points

--- cypress/integration/2.5-inactive_pickup_scenarios.spec.js
+++ cypress/integration/2.5-inactive_pickup_scenarios.spec.js
@@ -20,7 +20,7 @@ describe('Inactive Pickup Points should not be visible in storefront', () => {
 
   it(
     `Verify the inactive pickup point "${pickupPoint3Payload.name}" is not visible in stores page`,
-    updateRetry(1),
+    updateRetry(3),
     () => {
       cy.getPickupPointItem().then((pickupCount) => {
         cy.visitStore()
@@ -41,7 +41,7 @@ describe('Inactive Pickup Points should not be visible in storefront', () => {
 
   it('verify getStores with latitude and longitude', updateRetry(5), () => {
     graphql(storeLocator, getStores(-22.94, -43.18), (response) => {
-      cy.addDelayBetweenRetries(4000)
+      cy.addDelayBetweenRetries(10000)
       expect(response.status).to.equal(200)
       const pickupPoints = response.body.data.getStores.items.map((p) =>
         p.name.includes('pickup example')
