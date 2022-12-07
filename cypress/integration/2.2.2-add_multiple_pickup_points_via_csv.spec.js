27649dcaf5b9ea401f70980125d896dce0817945
Shashidhar Reddy <shashidhargujjula@gmail.com>
2022-12-07 17:33:39 +0530

    fixes

--- cypress/integration/2.2.2-add_multiple_pickup_points_via_csv.spec.js
+++ cypress/integration/2.2.2-add_multiple_pickup_points_via_csv.spec.js
@@ -1,7 +1,4 @@
 import { testSetup, updateRetry } from '../support/common/support'
-import { graphql } from '../support/common/graphql_utils'
-import { getStores } from '../support/graphql_testcase'
-import { storeLocator } from '../support/app_list'
 
 const prefix = 'Verify Upload XLS File'
 
@@ -13,25 +10,5 @@ describe('Adding Multiple pickup point', () => {
   // pickup example 1 & pickup example 2
   it(`${prefix} - Visit admin endpoint and upload file`, updateRetry(2), () => {
     cy.uploadXLS()
-  })
-
-  it('verify getStores with latitude and longitude', updateRetry(5), () => {
-    graphql(storeLocator, getStores(-22.94, -43.18), (response) => {
-      cy.addDelayBetweenRetries(10000)
-      expect(response.status).to.equal(200)
-      const pickupPoints = response.body.data.getStores.items.map((p) =>
-        p.name.includes('pickup example')
-      )
-
-      expect(pickupPoints.length).to.equal(2)
-    })
-  })
-
-  it('verify getStores without latitude and longitude', updateRetry(5), () => {
-    graphql(storeLocator, getStores(), (response) => {
-      cy.addDelayBetweenRetries(4000)
-      expect(response.status).to.equal(200)
-      expect(response.body.data.getStores.items.length).to.not.equal(0)
-    })
   })
 })
--- cypress/integration/2.5-inactive_pickup_scenarios.spec.js
+++ cypress/integration/2.5-inactive_pickup_scenarios.spec.js
@@ -7,6 +7,9 @@ import {
 import { restAPITestCase } from '../support/outputvalidation.js'
 import storelocatorSelectors from '../support/selectors.js'
 import { clickLoadAllStores } from '../support/common.js'
+import { graphql } from '../support/common/graphql_utils'
+import { getStores } from '../support/graphql_testcase'
+import { storeLocator } from '../support/app_list'
 
 const { pickupPoint3Payload } = restAPITestCase
 
@@ -37,4 +40,24 @@ describe('Inactive Pickup Points should not be visible in storefront', () => {
       })
     }
   )
+
+  it('verify getStores with latitude and longitude', updateRetry(5), () => {
+    graphql(storeLocator, getStores(-22.94, -43.18), (response) => {
+      cy.addDelayBetweenRetries(10000)
+      expect(response.status).to.equal(200)
+      const pickupPoints = response.body.data.getStores.items.map((p) =>
+        p.name.includes('pickup example')
+      )
+
+      expect(pickupPoints.length).to.equal(2)
+    })
+  })
+
+  it('verify getStores without latitude and longitude', updateRetry(5), () => {
+    graphql(storeLocator, getStores(), (response) => {
+      cy.addDelayBetweenRetries(4000)
+      expect(response.status).to.equal(200)
+      expect(response.body.data.getStores.items.length).to.not.equal(0)
+    })
+  })
 })
